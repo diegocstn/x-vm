9f52c669abefd1238572eb5d7d6c321a2b62d99b
Diego Costantino <diegocstn@gmail.com>
2017-07-20 22:19:40 +0200

    print vm status fn, inspect some issues

--- src/vm.c
+++ src/vm.c
@@ -4,7 +4,9 @@
 #include "isa.h"
 #include "stack.h"
 
-typedef enum { false, true } bool;
+typedef enum {
+  false = 0,
+  true = 1 } bool;
 
 typedef struct {
   int *data;
@@ -30,13 +32,22 @@ Vm *vm_init(int code[], int main, int datasize, int codesize) {
   machine->stack = calloc(STACK_SIZE, sizeof(int));
   machine->sp = -1;
   machine->ip = main;
+  machine->fp = main;
 
   return machine;
 }
 
+void vm_printstatus(Vm *vm, int opcode) {
+  fprintf( stderr, "%04d: %s", vm->ip, opname_from_code(opcode));
+  stack_print(vm->stack, STACK_SIZE);
+  fprintf(stderr, "\tfp=%d", vm->fp);
+  fprintf(stderr, "\tsp=%d", vm->sp);
+  fprintf(stderr, "\n");
+}
+
 void vm_exec(Vm *vm) { // TODO fix code data type
   int opcode;
-  int value;
+  int value1;
   int value2;
   int addr;
   int nargs;
@@ -49,36 +60,36 @@ void vm_exec(Vm *vm) { // TODO fix code data type
     switch (opcode) {
       case ICONST:
         // get the value and push it onto the stack
-        value = vm->code[vm->ip];
+        value1 = vm->code[vm->ip];
         vm->ip++;
         vm->sp++;
-        vm->stack[vm->sp] = value;
+        vm->stack[vm->sp] = value1;
         break;
       
       case PRINT:
-        value = vm->stack[vm->sp];
+        value1 = vm->stack[vm->sp];
         vm->sp--;
-        printf("%d\n", value);
+        printf("PRINT -> %d\n", value1);
         break;
 
       case GSTORE:
         // store value at addr of global store
-        value = vm->stack[vm->sp];
+        value1 = vm->stack[vm->sp];
         vm->sp--;
         addr = vm->code[vm->ip];
         vm->ip++;
-        vm->global[addr] = value;
+        vm->global[addr] = value1;
         break;
 
       case GLOAD:
         // get value from global 
         addr = vm->code[vm->ip];
         vm->ip++;
-        value = vm->global[addr];
+        value1 = vm->global[addr];
 
          // store it at the top of the stack
         vm->sp++;
-        vm->stack[vm->sp] = value;
+        vm->stack[vm->sp] = value1;
         break;
 
       case BR:
@@ -100,67 +111,84 @@ void vm_exec(Vm *vm) { // TODO fix code data type
         break;
 
       case ILT:
-        value2 = vm->code[vm->ip++];
-        value = vm->code[vm->ip++];
-        if (value < value2) {
-          vm->stack[vm->sp] = true;
-        } else {
-          vm->stack[vm->sp] = false;
-        }
+        value2 = vm->stack[vm->sp--];
+        value1 = vm->stack[vm->sp--];
+        vm->stack[++vm->sp] = value1 < value2 ? true : false;
+        break;
+
+      case IADD:
+        value2 = vm->stack[vm->sp--];
+        value1 = vm->stack[vm->sp--];
+        vm->stack[++vm->sp] = value1 + value2;
+        break;
+      
+      case ISUB:
+        value2 = vm->stack[vm->sp--];
+        value1 = vm->stack[vm->sp--];
+        vm->stack[++vm->sp] = value1 - value2;
+        break;
+
+      case IMUL:
+        value2 = vm->stack[vm->sp--];
+        value1 = vm->stack[vm->sp--];
+        vm->stack[++vm->sp] = value1 * value2;
         break;
 
       case LOAD:
         // get offset
-        value = vm->code[vm->ip++];
-        vm->sp++;
-        vm->stack[vm->sp] = vm->stack[vm->fp + value];
+        addr = vm->code[vm->ip++];
+        vm->stack[++vm->sp] = vm->stack[vm->fp + addr];
         break;
       
-      case CALL:
+      case CALL: {
         // we assume that args have been already pushed on the stack
-        addr = vm->code[vm->ip++];    // target addr function
-        nargs = vm->code[vm->ip++];   // how many args
-        vm->stack[vm->sp++] = nargs;   // save nargs on stack
-        vm->stack[vm->sp++] = vm->fp; // save current frame pointer
-        vm->stack[vm->sp++] = vm->sp;  // save current ip
+        addr = vm->code[vm->ip++];      // target addr function
+        nargs = vm->code[vm->ip++];     // how many args
 
-        vm->fp = vm->sp;              // make room for locals
-        vm->ip = addr;                // jump to the function
+        vm->stack[++vm->sp] = vm->fp;   // save current frame pointer
+        vm->stack[++vm->sp] = vm->ip;   // save current ip
+
+        vm->fp = vm->sp;                // make room for locals
+        int first_arg = vm->sp - nargs;
+        for (int i = 0; i < nargs; i++) {
+          vm->stack[vm->fp + i] = vm->stack[first_arg + i]; 
+        }
+        
+        vm->ip = addr;                  // jump to the function
 
         break;
+      }
 
       case RET:
-        value = vm->stack[vm->sp--];    // pop return value
+        value1 = vm->stack[vm->sp--];    // pop return value
         vm->sp = vm->fp;                // jump over locals
         vm->ip = vm->stack[vm->sp--];   // get return address
         vm->fp = vm->stack[vm->sp--];   // restore fp
+        printf("RET TO: %d\n", vm->sp);
+        return;
         
         nargs = vm->stack[vm->sp--];    // how many args do we need to throw away?
         vm->sp -= nargs;
 
         vm->sp++;
-        vm->stack[vm->sp] = value;      // save return value on stack
+        vm->stack[vm->sp] = value1;      // save return value on stack
         break;
 
 
       case HALT:
-        fprintf( stderr, "%04d: %s", vm->ip, opname_from_code(opcode));
-        stack_print(vm->stack, STACK_SIZE);
-        fprintf(stderr, "\n");
+        vm_printstatus(vm, opcode);
         return;
     }
     // trace
     if (vm->trace) {
-      fprintf( stderr, "%04d: %s", vm->ip, opname_from_code(opcode));
-      stack_print(vm->stack, STACK_SIZE);
-      fprintf(stderr, "\n");
+      vm_printstatus(vm, opcode);
     }
   }
 }
 
 int main() {
   // a beautiful factorial program
-  int program[] = {
+  int program_fact[] = {
                     // address
     LOAD, -3,       // 0
     ICONST, 2,      // 2
@@ -178,17 +206,30 @@ int main() {
 
 
     // main
-    ICONST, 5,  // 22
-    CALL, 0, 1, // 24
+    ICONST, 5,  // 21
+    CALL, 0, 1, // 23
     PRINT,      // 26
     HALT        // 27
   };
+  int main_ip_fact = 21;
 
-  int main_ip = 21;
+  int program_fun_call[] = {
+    LOAD, -3,     // 0
+    LOAD, -4,     // 2
+    IADD,         // 4
+    PRINT,        // 5
+    RET,          // 6
+    ICONST, 99,    // 7
+    ICONST, 123,    // 9
+    CALL, 0, 2,   // 10
+    HALT          // 14
+  };
+  int main_ip_fn_call = 7;
+
   int datasize = 1;
   int codesize = 28;
 
-  Vm *vm = vm_init(program, main_ip, datasize, codesize);
+  Vm *vm = vm_init(program_fun_call, main_ip_fn_call, datasize, codesize);
   vm->trace = true;
   vm_exec(vm);
 
